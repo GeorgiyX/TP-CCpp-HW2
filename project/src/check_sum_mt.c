8c882be46ecb3d1aae626ff5fecc6b06c1e230a4
Georgiy <dev.georgiy@gmail.com>
2021-04-04 00:09:02 +0300

    base code: mt realisation

--- project/src/check_sum_mt.c
+++ project/src/check_sum_mt.c
@@ -7,44 +7,16 @@
 #include "check_sum_private.h"
 #include "check_sum.h"
 
-#define PIPE_PAIR_SIZE 2
-const size_t WRITE_INDEX = 1;
-const size_t READ_INDEX = 0;
-
-int fork_task(int *arr, size_t array_size) {
-    exit(0);
-}
-
-int write_all(int fd, const char *buf, size_t size) {
-    if (!fd || !buf || !size || size >= PIPE_BUF) {
-        return EXIT_FAILURE;
-    }
-    int ret = 0;
-    while (size && ret > 0) {
-        ret = write(fd, buf, size);
-        size -= ret;
-        buf += ret;
-    }
-    return ret == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
-}
-
-int read_all(int fd, char *buf, size_t size) {
-    if (!fd || !buf || !size) {
-        return EXIT_FAILURE;
-    }
-    int ret = 1;
-    while (size && ret > 0) {
-        ret = read(fd, buf, size);
-        size -=ret;
-        buf +=ret;
-    }
-    return ret == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
-}
-
 #define read_all(fd, buf, size) io_all(fd, buf, size, read)
 #define write_all(fd, buf, size) io_all(fd, buf, size, (ssize_t (*)(int, void *, size_t)) write)
 
-int io_all(int fd, char *buf, size_t size, ssize_t (*io_function) (int, void *, size_t)) {
+#define PIPE_PAIR_SIZE 2
+extern const int CHECK_SUM_MODULE;
+const size_t WRITE_INDEX = 1;
+const size_t READ_INDEX = 0;
+
+
+int io_all(int fd, char *buf, size_t size, ssize_t (*io_function)(int, void *, size_t)) {
     if (!fd || !buf || !size || size >= PIPE_BUF) {
         return EXIT_FAILURE;
     }
@@ -55,6 +27,21 @@ int io_all(int fd, char *buf, size_t size, ssize_t (*io_function) (int, void *, 
         buf += ret;
     }
     return ret == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+void fork_work(int *arr, size_t array_size, int pipe_fd[2]) {
+    close(pipe_fd[READ_INDEX]);
+    int check_sum = 0;
+    int exit_code = EXIT_FAILURE;
+
+    do {
+        if (get_check_sum_private(arr, array_size, &check_sum)) { break; }
+        if (write_all(pipe_fd[WRITE_INDEX], (char *) &check_sum, sizeof(check_sum))) { break; }
+        exit_code = EXIT_SUCCESS;
+    } while (0);
+
+    close(pipe_fd[WRITE_INDEX]);
+    exit(exit_code);
 }
 
 int get_check_sum(int *arr, size_t array_size, int *check_sum) {
@@ -69,8 +56,8 @@ int get_check_sum(int *arr, size_t array_size, int *check_sum) {
 
     size_t num_of_forks = 0;
     int pipe_fd[PIPE_PAIR_SIZE];
+    int exit_code = EXIT_FAILURE;
     pid_t pid = 0;
-    int exit_status = 0;
 
     if (pipe(pipe_fd) == -1) {
         return EXIT_FAILURE;
@@ -81,49 +68,39 @@ int get_check_sum(int *arr, size_t array_size, int *check_sum) {
         if (pid == -1) {
             break;
         } else if (pid == 0) {
-            close(pipe_fd[READ_INDEX]);
-            int check_sum_fork = 0;
-            int return_code = get_check_sum_private(arr + num_of_forks * range_to_sum, range_to_sum, &check_sum_fork);
-            if (write_all(pipe_fd[WRITE_INDEX], (char *) &check_sum_fork, sizeof(check_sum_fork)) == -1) {
-                close(pipe_fd[WRITE_INDEX]);
-                exit(EXIT_FAILURE);
-            }
-            close(pipe_fd[WRITE_INDEX]);
-            exit(return_code);
+            fork_work(arr + num_of_forks * range_to_sum, range_to_sum, pipe_fd);
+        } else {
+            ++num_of_forks;
         }
-        ++num_of_forks;
     }
 
-    if (pid == -1) {
-        while (num_of_forks) {
-            wait(NULL);
-            --num_of_forks;
+    do {
+        if (pid == -1) { break; }
+
+        if (get_check_sum_private(arr + num_of_forks * range_to_sum,
+                                  (array_size - proc_count * range_to_sum), check_sum)) { break; }
+
+        int *check_sums = calloc(num_of_forks, sizeof(int));
+        if (!check_sums) { break; }
+        if (read_all(pipe_fd[READ_INDEX], (char *) check_sums, num_of_forks * sizeof(int))) {
+            free(check_sums);
+            break;
         }
-        return EXIT_FAILURE;
-    }
+        for (size_t i = 0; i < num_of_forks; ++i) {
+            *check_sum = (*check_sum + check_sums[i]) % CHECK_SUM_MODULE;
+        }
 
+        exit_code = EXIT_SUCCESS;
+    } while (0);
 
-    if (get_check_sum_private(arr + num_of_forks * range_to_sum,
-                              (array_size - proc_count * range_to_sum), check_sum) == EXIT_FAILURE) {
-        /* same as below */
-    }
-
-    int another_ret_code = 0;
-    int check_sum_from_process = 0;
-    while ((another_ret_code = read(pipe_fd[READ_INDEX], &check_sum_from_process, sizeof check_sum_from_process)) > 0) {
-
-    }
-
-
+    close(pipe_fd[READ_INDEX]);
+    close(pipe_fd[WRITE_INDEX]);
     int wait_status = 0;
     while (num_of_forks) {
-        wait(&wait_status);
-        if (WIFEXITED(wait_status) && WEXITSTATUS(wait_status) == EXIT_FAILURE) {
-            exit_status = EXIT_FAILURE;
+        if ((wait(&wait_status) == -1) || (WIFEXITED(wait_status) && WEXITSTATUS(wait_status) == EXIT_FAILURE)) {
+            exit_code = EXIT_FAILURE;
         }
         --num_of_forks;
     }
-
-
-    return get_check_sum_private(arr, array_size, check_sum);
+    return exit_code;
 }
